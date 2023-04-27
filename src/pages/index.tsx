853311e2163d0e046a47af326bd3dc10a4ec13a6
Jabo Fecht <jifecht@gmail.com>
2023-04-27 11:20:09 +0200

    Add synonyms, divider and more styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,27 +39,35 @@ export default function Home() {
 		}
 		if (isSuccess) {
 			return (
-				<div className="flex flex-col gap-5 shadow-lg p-8 rounded-lg bg-gray-200">
-					<p className="font-bold text-3xl">{data && data[0].word}</p>
-					<p className="font-bold text-3xl">
+				<div className="flex flex-col shadow-md p-8 rounded-lg bg-gray-200">
+					<p className="font-bold text-5xl mb-2">{data && data[0].word}</p>
+					<p className="text-2xl text-purple-700">
 						{data[0].phonetic && data[0].phonetic}
 					</p>
-					{data.map((entry, i) => (
-						<div key={i} className="mt-4">
-							{entry.meanings.map((meaning, i) => (
-								<div key={meaning.partOfSpeech + i}>
-									<p className="text-2xl font-bold my-4">
-										{meaning.partOfSpeech}
-									</p>
-									<ul className="list-disc list-outside flex flex-col gap-3 text-justify text-md pl-5">
-										{meaning.definitions?.slice(0, 3).map((definition, i) => (
-											<li key={i}>{definition.definition}</li>
-										))}
-									</ul>
+					<div className="mt-4 mb-4">
+						{data[0]?.meanings.map((meaning, i) => (
+							<div key={meaning.partOfSpeech + i} className="mt-4 mb-10">
+								<div className="flex w-full items-center justify-between mb-8">
+									<p className="text-2xl italic pr-6">{meaning.partOfSpeech}</p>
+									<hr className="bg-gray-300 opacity-100 dark:opacity-50 w-full h-0.5" />
 								</div>
-							))}
-						</div>
-					))}
+								<p className="text-lg font-light mb-4">Meaning</p>
+								<ul className="list-disc list-outside flex flex-col gap-3 text-justify text-md pl-7 mb-8">
+									{meaning.definitions?.slice(0, 3).map((definition, i) => (
+										<li key={i}>{definition.definition}</li>
+									))}
+								</ul>
+								{meaning.synonyms[0] && (
+									<div className="text-lg font-light">
+										Synonyms{" "}
+										<span className="text-purple-700 pl-5">
+											{meaning.synonyms}
+										</span>
+									</div>
+								)}
+							</div>
+						))}
+					</div>
 				</div>
 			)
 		}
@@ -70,12 +78,14 @@ export default function Home() {
 		<div className="flex flex-col items-center gap-2 pt-5 bg-gray-100 min-h-screen h-full">
 			<h1 className="text-xl">Search for a word: </h1>
 			<input
-				className="min-w-min max-w-1/5 h-8 shadow-lg border rounded-lg border-gray-400 px-3"
+				className="min-w-min w-2/3 md:w-1/2 xl:w-1/4 h-8 shadow-md border rounded-lg border-gray-200 px-3"
 				type="text"
 				value={searchValue}
 				onChange={handleChange}
 			/>
-			<div className="w-2/3 md:w-1/2 xl:w-1/4 m-3">{renderResult()}</div>
+			<div className="xs:w-max sm:w-3/4 md:w-1/2 2xl:w-1/3 m-3">
+				{renderResult()}
+			</div>
 		</div>
 	)
 }
